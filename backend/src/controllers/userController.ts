1b8131b97ed31c09d42ba50c6d49207ac10988ab
Ritsuki <cxlm@cxlm.work>
2025-09-24 22:38:41 +0800

    基本模型定义、用户API实现

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express'
 import * as userService from '../services/userService'
 
+const REQ_HEADER_TOKEN = 'x-auth-token'
+
 export const getUserById = async (req: Request, res: Response) => {
   try {
     const uid = parseInt(req.params.uid, 10)
     if (isNaN(uid)) {
-      return res.status(400).json({ message: 'Invalid user ID' })
+      return res.status(400).json({ message: '非法用户ID' })
+    }
+
+    // 拦截未登录请求（Token）
+    const requestToken = req.headers[REQ_HEADER_TOKEN] as string
+    if (!requestToken) {
+      return res.status(401).json({ message: '请登录' })
+    }
+    // 校验 Token
+    if (userService.invalidateToken(requestToken)) {
+      return res.status(401).json({ message: '登录已过期，请重新登录' })
     }
 
     // 权限检查的预留位置
@@ -17,12 +29,33 @@ export const getUserById = async (req: Request, res: Response) => {
     const user = await userService.findUserById(uid)
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' })
+      return res.status(404).json({ message: '用户未找到' })
     }
 
     return res.status(200).json(user)
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ message: 'Internal server error' })
+    return res.status(500).json({ message: '内部服务器错误' })
+  }
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body
+    if (!username || !password) {
+      return res.status(400).json({ message: '必须提供用户名或密码' })
+    }
+
+    const user = await userService.findUserByCredentials(username, password)
+    if (!user) {
+      return res.status(401).json({ message: '用户名或密码错误' })
+    }
+
+    // 生成并返回JWT
+    const token = userService.generateToken(user)
+    return res.status(200).json({ token })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: '内部服务器错误' })
   }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,4 +6,43 @@ export interface User {
   role: UserRole
   point: number
   // pwd: string; // 密码不在这里暴露
+  createTime: Date
+  updateTime: Date
+}
+
+export type TaskFrequency = 'once' | 'daily' | 'workday' | 'weekend' | 'endless'
+export type TaskStatus = 'editing' | 'active' | 'completed' | 'expired'
+// export type TaskType = # 忘记了类型
+
+export interface Task {
+  id: number
+  title: string
+  desc: string
+  freq: TaskFrequency
+  status: TaskStatus
+  createdBy: number // User ID
+  // type: TaskType
+  rewardId: number // Reward ID
+  deadline: Date | null
+  createTime: Date
+  updateTime: Date
+}
+
+export interface Reward {
+  id: number
+  fx: string // 奖励计算函数代码
+  maxInput: number // 奖励计算函数的最大输入值
+  minInput: number // 奖励计算函数的最小输入值
+  createTime: Date
+  updateTime: Date
+}
+
+export interface TaskRecord {
+  id: number
+  taskId: number
+  userId: number
+  score: number // 任务完成时的评分（1-5）
+  pointsAwarded: number // 实际奖励的积分
+  createTime: Date // 视为完成时间
+  updateTime: Date
 }
